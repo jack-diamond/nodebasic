94a1ccb738f0542c5b243bc8eddbf25cc4abf38a
jack-diamond <jhdiamond96@gmail.com>
2019-12-26 15:57:47 -0800

    Using Express now

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+  console.log(__dirname);
+})
+
+app.get('/index', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname + '/about.html'));
+})
+
+app.get('/contact-me', (req, res) => {
+  res.sendFile(path.join(__dirname + '/contact-me.html'));
+})
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname + '/404.html'));
+})
+
+app.listen(8080);
